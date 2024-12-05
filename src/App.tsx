ea0ac56f5591fe86fa53e1bd8abbd2655f885244
Hai Cao <caothehai1991@gmail.com>
2024-12-05 09:29:54 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,10 @@
 import React from 'react';
-import Profile from './components/Profile';
 import "./App.css";
 
 function App() {
-  const tasks = ['Learn React', 'Build an App', 'Enjoy Coding'];
-  const age = 18;
-  const name = 'Cao Hai';
-
   return (
     <div>
       <h1>My To-Do List</h1>
-      <div>{name} - {age}</div>
-      <div>{tasks}</div>
-      <ul>
-        {tasks.map((task, index) => (
-          <li key={index}>{task}</li>
-        ))}
-      </ul>
-      <Profile />
     </div>
   );
 }
