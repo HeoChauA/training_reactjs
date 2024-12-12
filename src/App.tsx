a8b8e43513d15e3ba4802a93b6820717f623d463
Nhung Le <nhung.le@ffwagency.com>
2024-12-12 16:20:22 +0700

    NHUNG: Add react router, add pages and update styling.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,55 @@
 import React from 'react';
 import "./App.css";
 import Todos from './components/Todos/Todos';
+import Profile from './components/Profile/Profile';
+
+import Header from './components/Header/Header';
+import { Link, Routes, Route } from "react-router";
+
+const todos = [
+  {
+    title: "Todo title 1",
+    desc: "Suspendisse enim turpis, dictum sed, iaculis a, condimentum nec, nisi. Sed mollis, eros et ultrices tempus, mauris ipsum aliquam libero, non adipiscing dolor urna a orci. Fusce fermentum. Pellentesque egestas, neque sit amet convallis pulvinar, justo nulla eleifend augue, ac auctor orci leo non est. Suspendisse feugiat.",
+    isCompleted: true
+  },
+  {
+    title: "Todo title 2",
+    desc: "Praesent nec nisl a purus blandit viverra. Maecenas vestibulum mollis diam. In consectetuer turpis ut velit. Sed in libero ut nibh placerat accumsan. Integer tincidunt.",
+    isCompleted: true
+  },
+  {
+    title: "Todo title 3",
+    desc: "Vivamus in erat ut urna cursus vestibulum. Vestibulum suscipit nulla quis orci. Donec venenatis vulputate lorem. Vivamus euismod mauris. Fusce vel dui.",
+    isCompleted: false
+  },
+  {
+    title: "Todo title 4",
+    desc: "Donec vitae sapien ut libero venenatis faucibus. Vivamus elementum semper nisi. Aliquam erat volutpat. Suspendisse feugiat. In hac habitasse platea dictumst.",
+    isCompleted: false
+  },
+  {
+    title: "Todo title 5",
+    desc: " Donec posuere vulputate arcu. Suspendisse feugiat. Nulla sit amet est. Cras risus ipsum, faucibus ut, ullamcorper id, varius ac, leo.",
+    isCompleted: false
+  }
+];
 
 function App() {
   return (
-    <div className='container'>
-      <h1 style={{ textAlign: 'center' }}>My To-Do List</h1>
-      <Todos />
+    <div className='main'>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Todos todos={todos}/>}/>
+        <Route 
+          path='/profile' 
+          element={
+            <Profile 
+              name = "Le Nhung"
+              email = "nhung.cntt.19a3@gmail.com"
+              age = {18}
+              adress = "Ninh Binh city"
+          />}/>
+      </Routes>
     </div>
   );
 }
--- src/components/Todos/Todos.tsx
+++ src/components/Todos/Todos.tsx
@@ -2,37 +2,27 @@ import { FaEdit } from "react-icons/fa";
 import { MdDelete } from "react-icons/md";
 import { FaEye } from "react-icons/fa";
 
-const todos = [
-  {
-    title: "Todo title 1",
-    desc: "Suspendisse enim turpis, dictum sed, iaculis a, condimentum nec, nisi. Sed mollis, eros et ultrices tempus, mauris ipsum aliquam libero, non adipiscing dolor urna a orci. Fusce fermentum. Pellentesque egestas, neque sit amet convallis pulvinar, justo nulla eleifend augue, ac auctor orci leo non est. Suspendisse feugiat."
-  },
-  {
-    title: "Todo title 2",
-    desc: "Praesent nec nisl a purus blandit viverra. Maecenas vestibulum mollis diam. In consectetuer turpis ut velit. Sed in libero ut nibh placerat accumsan. Integer tincidunt."
-  },
-  {
-    title: "Todo title 3",
-    desc: "Vivamus in erat ut urna cursus vestibulum. Vestibulum suscipit nulla quis orci. Donec venenatis vulputate lorem. Vivamus euismod mauris. Fusce vel dui."
-  },
-  {
-    title: "Todo title 4",
-    desc: "Donec vitae sapien ut libero venenatis faucibus. Vivamus elementum semper nisi. Aliquam erat volutpat. Suspendisse feugiat. In hac habitasse platea dictumst."
-  },
-  {
-    title: "Todo title 5",
-    desc: " Donec posuere vulputate arcu. Suspendisse feugiat. Nulla sit amet est. Cras risus ipsum, faucibus ut, ullamcorper id, varius ac, leo."
-  }
-];
 
 type TodoItemType = {
   title: string;
   desc: string;
+  isCompleted: boolean;
 };
 
-const TodoItem = ({title, desc}: TodoItemType) => {
+type TodosType = {
+  todos: TodoItemType[];
+};
+
+
+const TodoItem = ({title, desc, isCompleted}: TodoItemType) => {
+  let isCompletedClasses;
+
+  if (isCompleted) {
+    isCompletedClasses = 'green-bg';
+  }
+
   return (
-    <li>
+    <li className={isCompletedClasses}>
       <div className="todo-content">
         <h3 className="todo-title">{title}</h3>
         <p className="todo-desc">{desc}</p>
@@ -46,18 +36,21 @@ const TodoItem = ({title, desc}: TodoItemType) => {
   )
 }
 
-const Todos = () => {
+const Todos = ({ todos }: TodosType) => {
   return (
-    <div className="todos">
-      <div className="todos__inner">
+    <>
+      <h1 style={{ textAlign: "center" }}>My todo list</h1>
+      <div className="todos">
+        <div className="container">
         <button className="todos__button">Add Todo</button>
         <ul className="todos__list">
           {todos.map((todo) => (
-            <TodoItem title={todo.title} desc={todo.desc} />
+            <TodoItem title={todo.title} desc={todo.desc} isCompleted={todo.isCompleted}/>
           ))}
         </ul>
       </div>
     </div>
+    </>
   )
 }
 
