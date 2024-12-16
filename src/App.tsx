e0ec2983b5b33e8117d2e3b7519e264912f22f4e
Hai Cao <caothehai1991@gmail.com>
2024-12-16 22:46:07 +0700

    update router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import "./App.css";
+import Profile from './components/Profile';
 
 function App() {
   return (
-    <div>
-      <h1>My To-Do List</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<h1>My To-Do List</h1>} />
+        <Route path='/profile' element={<Profile />} />
+      </Routes>
+    </Router>
   );
 }
 
